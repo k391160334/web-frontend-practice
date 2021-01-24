cae00a1bc54554a34253a57a77025bf4f6ff18ec
k391160334 <maseqqqq@gmail.com>
2021-01-25 06:30:23 +0900

    clock update edit

--- src/clock.js
+++ src/clock.js
@@ -1,12 +1,10 @@
 const clock=document.querySelector(".js-clock");
-const today=new Date();
 
 function init(){
-  const hour=today.getHours();
-  const minute=today.getMinutes();
   setInterval(getTime, 1000);
 }
 function getTime(){
+  const today=new Date();
   const hour=today.getHours();
   const minute=today.getMinutes();
   const clockStr=`${hour<10?`0${hour}`:hour
